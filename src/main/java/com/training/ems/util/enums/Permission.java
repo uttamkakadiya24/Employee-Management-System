68c6846df01d83a3fde50f925dd15f071c560ead
Uttam Kakadiya <uttamkakadiya1029@gmail.com>
2024-04-04 16:42:18 +0530

    #5 : Basic Change Commit

---
+++ src/main/java/com/training/ems/util/enums/Permission.java
@@ -0,0 +1,15 @@
+package com.training.ems.util.enums;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum Permission {
+
+    READ,
+    CREATE,
+    UPDATE,
+    DELETE
+
+}
